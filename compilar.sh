0d891ceb1646050791822dd547d84467998bb04a
acidrums4 <acidrums4@gmail.com>
2017-07-10 19:04:19 -0500

    Agregar experiencia en Wordpress.

--- compilar.sh
+++ compilar.sh
@@ -6,7 +6,7 @@
 # Ejemplo:
 # ./compilar.sh invoice
 
-export numero='002'
+export numero='003'
 export fecha=`date "+%d%m%y"`
 
 context -mode=${1} -arguments=numero=${numero},fecha=${fecha} -result=invoice_${numero}-${fecha} curriculum.tex
