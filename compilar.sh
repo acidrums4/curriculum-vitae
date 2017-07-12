097716e1c73c3565ed1953c618888593f5d53b3c
acidrums4 <acidrums4@gmail.com>
2017-07-12 16:27:57 -0500

    Idioma, intereses. Ajustes menores.

--- compilar.sh
+++ compilar.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 
-# Modo factura:    invoice
+# Modo factura:    factura
 # Modo cotización: cotizar
 
 # Ejemplo:
-# ./compilar.sh invoice
+# ./compilar.sh factura
 
 export numero='003'
 export fecha=`date "+%d%m%y"`
 
-context -mode=${1} -arguments=numero=${numero},fecha=${fecha} -result=invoice_${numero}-${fecha} curriculum.tex
+context -mode=${1} -arguments=numero=${numero},fecha=${fecha} -result=${1}_${numero}-${fecha} curriculum.tex
