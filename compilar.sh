8d1b2c39da3431ab9c5ca9960d435ecbe30bfa3d
acidrums4 <acidrums4@gmail.com>
2018-01-10 19:34:11 -0500

    Versión b/n.

--- compilar.sh
+++ compilar.sh
@@ -6,7 +6,7 @@
 # Ejemplo:
 # ./compilar.sh factura
 
-export numero='004'
+export numero='005'
 export fecha=`date "+%d%m%y"`
 
 context -mode=${1} -arguments=numero=${numero},fecha=${fecha} -result=${1}_${numero}-${fecha} curriculum.tex
